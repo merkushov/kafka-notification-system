3bc7418dff7257e463d1c3ad104eb55cc721cc1d
Victor Merkushov <merkushov.victor@gmail.com>
2025-03-23 18:02:48 +0600

    added Swagger to the Producer

--- apps/producer-service/src/main.ts
+++ apps/producer-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ProducerServiceModule } from "./producer-service.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(ProducerServiceModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Producer Service API")
+    .setDescription("The Producer Service API description")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
